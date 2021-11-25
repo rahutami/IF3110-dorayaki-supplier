0cc7595ad944c5cd6666d7b578fda242d5a3a30c
Shafira Naya <shfrnaya@gmail.com>
2021-11-26 00:40:56 +0700

    edit getvariant, change status dipesan to waiting

--- src/main/java/com/dorayakisupplier/repository/RequestRepository.java
+++ src/main/java/com/dorayakisupplier/repository/RequestRepository.java
@@ -9,7 +9,7 @@ public class RequestRepository {
     public static final Connection conn = DatabaseConnection.getConnection();
 
     public int insertRequest(RequestDorayaki reqDorayaki) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("INSERT into request(id_dorayaki, jumlah, status, timestamp) value(?, ?, 'dipesan', ?)");
+        PreparedStatement ps = conn.prepareStatement("INSERT into request(id_dorayaki, jumlah, status, timestamp) value(?, ?, 'waiting', ?)");
         ps.setInt(1, reqDorayaki.getIdDorayaki());
         ps.setInt(2, reqDorayaki.getAmount());
         ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
--- src/main/java/com/dorayakisupplier/service/GetVariantService.java
+++ src/main/java/com/dorayakisupplier/service/GetVariantService.java
@@ -1,10 +1,15 @@
 package com.dorayakisupplier.service;
-import com.dorayakisupplier.repository.VariantRepository;
+import com.dorayakisupplier.model.LogRequest;
+import com.dorayakisupplier.model.RequestDorayaki;
 import com.dorayakisupplier.model.Variant;
+import com.dorayakisupplier.repository.VariantRepository;
+import com.dorayakisupplier.repository.LogRequestRepository;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +17,27 @@ import java.util.List;
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public class GetVariantService {
     private static final VariantRepository variantRepository = new VariantRepository();
+    private static final LogRequestRepository logRequestRepository = new LogRequestRepository();
 
     @WebMethod
-    public List<Variant> getAllVariant() {
-        try {
-            return variantRepository.getAllVariant();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ArrayList<>();
+    public List<Variant> getAllVariant(String ipAddress) {
+        // bingung masalah ipAddress
+        try{
+            if(logRequestRepository.countLog(ipAddress, "/variant") > 10) return new ArrayList<>();
+            int resultLog = logRequestRepository.insertLog(new LogRequest(ipAddress, Timestamp.valueOf(LocalDateTime.now()), "/variant"));
+            if (resultLog == 1) return variantRepository.getAllVariant();
+            else return new ArrayList<>();
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
         }
+        return new ArrayList<>();
+
+//        try {
+//            return variantRepository.getAllVariant();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return new ArrayList<>();
+//        }
     }
+
 }
